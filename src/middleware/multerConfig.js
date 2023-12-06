ee88bc0854c062ffa8ab9d1cb79269d0ac42a017
Muhammad Noman <m.nouman.nomi@gmail.com>
2023-12-06 22:16:59 +0500

    fix issue in upload Image

--- src/middleware/multerConfig.js
+++ src/middleware/multerConfig.js
@@ -1,14 +1,10 @@
 import multer from "multer";
 import path from "path";
-import { fileURLToPath } from "url";
 
 const storage = multer.diskStorage({
   destination: "./public/images/",
   filename: (req, file, cb) => {
-    cb(
-      null,
-      Date.now() + "-" + file.fieldname + path.extname(file.originalname)
-    );
+    cb(null, Date.now() + "-" + file.fieldname + "-" + file.originalname);
   },
 });
 const upload = multer({ storage });
