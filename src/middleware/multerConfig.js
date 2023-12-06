0fc460555a9c84f5074e42170a23352c8f21d413
Muhammad Noman <m.nouman.nomi@gmail.com>
2023-12-06 15:05:26 +0500

    fix issue in upload Image

--- src/middleware/multerConfig.js
+++ src/middleware/multerConfig.js
@@ -2,16 +2,13 @@ import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Use fileURLToPath to convert import.meta.url to a file path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.resolve(__dirname, "../public/uploads"));
-  },
+  destination: "./public/images/",
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(
+      null,
+      Date.now() + "-" + file.fieldname + path.extname(file.originalname)
+    );
   },
 });
 const upload = multer({ storage });
